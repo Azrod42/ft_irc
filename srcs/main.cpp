e5d468525158e5e6d3adf1f3c8388326c1988fb0
azrod42 <tom.sorabella@gmail.com>
2023-02-27 16:44:14 +0100

    Autopush Makefile

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,7 @@
 #include"../includes/ft_irc.hpp"
 
 #define IP INADDR_ANY
+#define BACKLOG 3
 
 void init_sockaddr(struct sockaddr_in *addr, unsigned short port){
 	addr->sin_family = AF_INET;
@@ -8,11 +9,28 @@ void init_sockaddr(struct sockaddr_in *addr, unsigned short port){
 	addr->sin_port = htons(port);
 }
 
+int	init_socket(t_data *dta, struct sockaddr_in *addr) {
+	int	number_option;
+
+	dta->fd_socket = -1;
+	if ((dta->fd_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1){
+		std::cout << "Error : During socket creation" << std::endl; return (1);}
+	if (setsockopt(dta->fd_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &number_option, sizeof(number_option)) != 0){
+		std::cout << "Error : During socket configuration" << std::endl; return (1);}
+	if (bind(dta->fd_socket, (struct sockaddr *)addr, sizeof(addr)) != 0){
+		std::cout << "Error : During dind on IP/port" << std::endl; return (1);}
+	if (listen(dta->fd_socket, BACKLOG) != 0){
+		std::cout << "Error : During listen" << std::endl; return (1);}
+	fcntl(dta->fd_socket, F_SETFL, O_NONBLOCK);
+	std::cout << "Socket mis en ecoute" << std::endl;
+	return (0);
+}
+
 int pars_port(t_data *dta, char **argv){
 	std::string port(argv[1]);
 	
 	if (port.size() < 0 || port.size() > 5) {	
-		std::cout << "error : Invalid port" << std::endl; return 1;}
+		std::cout << "error : Port range not valid" << std::endl; return 1;}
 	dta->port = static_cast<unsigned short>(std::atoi(argv[1]));
 	return (0);
 }
