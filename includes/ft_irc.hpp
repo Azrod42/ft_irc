657f76fa421a06427620235d08d6d51e9203787d
azrod42 <tom.sorabella@gmail.com>
2023-02-27 13:57:29 +0100

    Autopush Makefile

--- includes/ft_irc.hpp
+++ includes/ft_irc.hpp
@@ -1,6 +1,20 @@
 #ifndef FT_IRC_HPP
 #define FT_IRC_HPP
 
-#include<iostream>
+# include<iostream>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+
+typedef struct s_data {
+	unsigned short	port;
+	int				fd_socket;
+} t_data;
 
 #endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,39 @@
 #include"../includes/ft_irc.hpp"
 
-int main(void)
+#define IP INADDR_ANY
+
+void init_sockaddr(struct sockaddr_in *addr, unsigned short port){
+	addr->sin_family = AF_INET;
+	addr->sin_addr.s_addr = IP;
+	addr->sin_port = htons(port);
+}
+
+int pars_port(t_data *dta, char **argv){
+	std::string port(argv[1]);
+	
+	if (port.size() < 0 || port.size() > 5) {	
+		std::cout << "error : Invalid port" << std::endl; return 1;}
+	dta->port = static_cast<unsigned short>(std::atoi(argv[1]));
+	return (0);
+}
+
+int main(int argc, char **argv)
 {
-	std::cout << "test" << std::endl;
+	t_data dta;
+	struct sockaddr_in adresse;
+
+	if (argc != 3) {
+		std::cout << "Usage : ./ircserv [port] [password]" << std::endl;
+		return (1);
+	}
+	if (pars_port(&dta, argv)) return (1);
+	init_sockaddr(&adresse, dta.port);
+	// int fd_socket = socket(AF_INET, SOCK_STREAM, 0);
+	// setsockopt(fd_socket, IPPROTO_TCP, SO_REUSEADDR, );
+	// bind(fd_socket, const struct sockaddr *addr, socklen_t addrlen)
+
+
+	std::cout << adresse.sin_port << std::endl;
+	std::cout << argv[1] << " " << argv[2] << std::endl;
+	return (0);
 }
